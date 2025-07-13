5b2d46ccd0dae5291e34d89afc3df068e57e27ae
Peter Marks <pmarks@signiant.com>
2025-07-13 10:35:02 -0400

    fix profile

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -4,43 +4,38 @@
   programs.vscode = {
     enable = true;
 
-    profiles = {
-      default = {
-        name = "Default";
-        description = "Default profile with common settings and extensions.";
-
-        extensions = (with pkgs.vscode-extensions; [
-          bbenoist.nix
-          vscode-icons-team.vscode-icons
-          ms-python.python
-          esbenp.prettier-vscode
-          dbaeumer.vscode-eslint
-        ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            publisher = "tomphilbin";
-            name = "gruvbox-themes";
-            version = "1.0.0";
-          }
-        ];
-      
-        userSettings = {
-          editor = {
-            tabSize = 2;
-            formatOnSave = true;
-            fontFamily = "FiraCode Nerd Font";
-            fontSize = 18;
-            fontLigatures = true;
-            inlineSuggest.enabled = true;
-            bracketPairColorization.enabled = true;
-          };
-
-          workbench = {
-            colorTheme = "Gruvbox Dark (Soft)";
-            iconTheme = "vscode-icons";
-          };
-
-          terminal.integrated.fontSize = 18;
+    profiles.default = {
+      extensions = (with pkgs.vscode-extensions; [
+        bbenoist.nix
+        vscode-icons-team.vscode-icons
+        ms-python.python
+        esbenp.prettier-vscode
+        dbaeumer.vscode-eslint
+      ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          publisher = "tomphilbin";
+          name = "gruvbox-themes";
+          version = "1.0.0";
+        }
+      ];
+    
+      userSettings = {
+        editor = {
+          tabSize = 2;
+          formatOnSave = true;
+          fontFamily = "FiraCode Nerd Font";
+          fontSize = 18;
+          fontLigatures = true;
+          inlineSuggest.enabled = true;
+          bracketPairColorization.enabled = true;
         };
+
+        workbench = {
+          colorTheme = "Gruvbox Dark (Soft)";
+          iconTheme = "vscode-icons";
+        };
+
+        terminal.integrated.fontSize = 18;
       };
     };
   };
