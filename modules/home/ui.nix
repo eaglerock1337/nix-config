71a069e38dcff97fe926e72094c2d7c1f8984574
Peter Marks <pmarks@signiant.com>
2025-07-13 09:02:04 -0400

    start cleaning up color configs

--- modules/home/ui.nix
+++ modules/home/ui.nix
@@ -2,18 +2,6 @@
 
 let
   nerdFont = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
-  gruvboxDark = {
-    bg = "#282828";
-    fg = "#ebdbb2";
-    black = "#1d2021";
-    red = "#fb4934";
-    green = "#b8bb26";
-    yellow = "#fabd2f";
-    blue = "#83a598";
-    purple = "#d3869b";
-    aqua = "#8ec07c";
-    gray = "#a89984";
-  };
 in {
   # ROFI Config
   programs.rofi = {
@@ -35,33 +23,8 @@ in {
 
   programs.alacritty = {
     enable = true;
+    theme = "gruvbox-dark";
     settings = {
-      colors = {
-        primary = {
-          background = gruvboxDark.bg;
-          foreground = gruvboxDark.fg;
-        };
-        normal = {
-          black = gruvboxDark.black;
-          red = gruvboxDark.red;
-          green = gruvboxDark.green;
-          yellow = gruvboxDark.yellow;
-          blue = gruvboxDark.blue;
-          magenta = gruvboxDark.purple;
-          cyan = gruvboxDark.aqua;
-          white = gruvboxDark.fg;
-        };
-        bright = {
-          black = "#928374";
-          red = "#fb4934";
-          green = "#b8bb26";
-          yellow = "#fabd2f";
-          blue = "#83a598";
-          magenta = "#d3869b";
-          cyan = "#8ec07c";
-          white = "#ebdbb2";
-        };
-      };
       font = {
         normal.family = "FiraCode Nerd Font";
         size = 11;
