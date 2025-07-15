1eef65d191be99ae09977bee00d07fe01bd7295b
Peter Marks <pmarks@signiant.com>
2025-07-14 20:52:13 -0400

    fix

--- modules/hosts/grub.nix
+++ modules/hosts/grub.nix
@@ -1,13 +1,16 @@
 { config, lib, pkgs, ... }:
 
 let
-  grubThemePath = ./grub;
+  grubAssets = "../../assets/grub";
   gruvboxTheme = pkgs.runCommand "grub-theme" {
-    inherit grubThemePath;
-  }
-  ''
+    themeTxt = "${grubAssets}/theme.txt";
+    background = "${grubAssets}/grub-wallpaper.png";
+    font = "${grubAssets}/FiraSans-Regular.pf2";
+  } ''
     mkdir -p "$out"
-    cp -r "$grubThemePath/." "$out/"
+    cp "$themeTxt" "$out/theme.txt"
+    cp "$background" "$out/background.png"
+    cp "$font" "$out/converted-font.pf2"
   '';
 in {
   boot.loader.systemd-boot.enable = false;
