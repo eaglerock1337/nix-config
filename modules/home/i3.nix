36764cb1e9b19a7c7f63fe4a0c0866603be89629
Peter Marks <pmarks@signiant.com>
2025-07-15 10:09:18 -0400

    forget this nonsense for now

--- modules/home/i3.nix
+++ modules/home/i3.nix
@@ -285,7 +285,7 @@ in {
           notification = false;
         }
         {
-          command = ''i3-msg "exec alacritty --class Scratchpad --command sh -c 'neofetch; exec bash'"'';
+          command = ''i3-msg "exec alacritty --class Scratchpad"'';
           always = false;
           notification = false;
         }
