793ae2d703976245f87c180b3ff200612b81cffa
Peter Marks <pmarks@signiant.com>
2025-07-14 21:37:27 -0400

    this?

--- modules/hosts/grub.nix
+++ modules/hosts/grub.nix
@@ -2,15 +2,30 @@
 
 let
   themeSource = ./grub; # dir containing theme.txt, png, pf2
+  gruvboxTheme = pkgs.stdenv.mkDerivation {
+    name = "gruvbox-grub-theme";
+    # Point src at the folder with theme.txt, background.png, converted-font.pf2
+    src = ./grub;
+
+    # We don’t need a build; just an install phase
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out
+    '';
+
+    # Optional metadata
+    meta.description = "Gruvbox‐style GRUB theme";
+  };
 in {
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     gfxmodeEfi = "auto";
     device = "nodev";
-    splashImage = "${themeSource}/grub-wallpaper.png";
-    font = "${themeSource}/FiraCode-Regular.pf2";
-    theme = "${themeSource}/theme.txt";
+    splashImage = "${gruvboxTheme}/grub-wallpaper.png";
+    font = "${gruvboxTheme}/FiraCode-Regular.pf2";
+    theme = "${gruvboxTheme}/theme.txt";
   };
 
   boot.loader.systemd-boot.enable = false;
