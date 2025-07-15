ddaa7879ae197e1bf5e041179c14ca66e747c20c
Peter Marks <pmarks@signiant.com>
2025-07-14 20:22:30 -0400

    try this

--- modules/hosts/grub.nix
+++ modules/hosts/grub.nix
@@ -1,16 +1,21 @@
 { config, lib, pkgs, ... }:
 
 let
-  dir = builtins.toString ./.;
-  grubTheme = "${dir}/grub/theme.txt";
+  # dir = builtins.toString ./.;
+  # grubTheme = "${dir}/grub/theme.txt";
+  gruvboxTheme = pkgs.runCommand "grub-theme" {} ''
+    mkdir -p $out
+    cp -r ${grubThemePath}/* $out/
+  '';
 in {
   boot.loader.systemd-boot.enable = false;
+
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     gfxmodeEfi = "auto";
     device = "nodev";
-    theme = builtins.toString grubTheme;
+    theme = ${gruvboxTheme}/theme.txt;
   };
 
   boot.loader.efi.canTouchEfiVariables = true;
