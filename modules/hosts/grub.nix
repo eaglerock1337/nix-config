56a6cfaee5ea09f80644e1fe3b932b2e6fca1d24
Peter Marks <pmarks@signiant.com>
2025-07-14 21:12:00 -0400

    try again

--- modules/hosts/grub.nix
+++ modules/hosts/grub.nix
@@ -4,20 +4,14 @@ let
   themeSource = ./grub; # dir containing theme.txt, png, pf2
 in {
   # Install theme in /etc
-  environment.boot."grub/themes/gruvbox".source = themeSource;
-
-  # # Copy to /boot after build
-  # boot.postBootCommands = ''
-  #   mkdir -p /boot/grub/themes/gruvbox
-  #   cp -r /etc/grub/themes/gruvbox/* /boot/grub/themes/gruvbox/
-  # '';
+  environment.etc."grub/themes/gruvbox".source = themeSource;
 
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     gfxmodeEfi = "auto";
     device = "nodev";
-    theme = "/boot/grub/themes/gruvbox/theme.txt";
+    theme = "/etc/grub/themes/gruvbox/theme.txt";
   };
 
   boot.loader.systemd-boot.enable = false;
