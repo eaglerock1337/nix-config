3ab64ee418ec9ff40eaa9909ec8f89f4ddba2e2f
Peter Marks <pmarks@signiant.com>
2025-07-15 09:21:55 -0400

    asdf

--- modules/home/i3.nix
+++ modules/home/i3.nix
@@ -242,7 +242,7 @@ in {
 
       startup = [
         {
-          command = "xrandr --output eDP-1 --scale 1.5x1.5 --dpi 144"
+          command = "xrandr --output eDP-1 --scale 1.5x1.5 --dpi 144";
         }
         {
           command = "systemctl --user restart polybar.service";
