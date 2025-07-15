cc8caf2452409b75b1e1295cc78c87b886aa95cc
Peter Marks <pmarks@signiant.com>
2025-07-14 21:10:48 -0400

    bah

--- modules/hosts/grub.nix
+++ modules/hosts/grub.nix
@@ -4,13 +4,13 @@ let
   themeSource = ./grub; # dir containing theme.txt, png, pf2
 in {
   # Install theme in /etc
-  environment.etc."grub/themes/gruvbox".source = themeSource;
+  environment.boot."grub/themes/gruvbox".source = themeSource;
 
-  # Copy to /boot after build
-  boot.postBootCommands = ''
-    mkdir -p /boot/grub/themes/gruvbox
-    cp -r /etc/grub/themes/gruvbox/* /boot/grub/themes/gruvbox/
-  '';
+  # # Copy to /boot after build
+  # boot.postBootCommands = ''
+  #   mkdir -p /boot/grub/themes/gruvbox
+  #   cp -r /etc/grub/themes/gruvbox/* /boot/grub/themes/gruvbox/
+  # '';
 
   boot.loader.grub = {
     enable = true;
