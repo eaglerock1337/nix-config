0a6b4250580e9ffd413ef120114389953056a5e3
Peter Marks <pmarks@signiant.com>
2025-07-14 22:10:00 -0400

    hope

--- modules/hosts/grub.nix
+++ modules/hosts/grub.nix
@@ -2,45 +2,45 @@
 
 let
   themeSource = ./grub; # dir containing theme.txt, png, pf2
-  gruvboxTheme = pkgs.stdenv.mkDerivation {
-    name = "gruvbox-grub-theme";
-    # Point src at the folder with theme.txt, background.png, converted-font.pf2
-    src = ./grub;
+  # gruvboxTheme = pkgs.stdenv.mkDerivation {
+  #   name = "gruvbox-grub-theme";
+  #   # Point src at the folder with theme.txt, background.png, converted-font.pf2
+  #   src = ./grub;
 
-    # We don’t need a build; just an install phase
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out
-      cp -a $src/. $out
-    '';
+  #   # We don’t need a build; just an install phase
+  #   phases = [ "installPhase" ];
+  #   installPhase = ''
+  #     mkdir -p $out
+  #     cp -a $src/. $out
+  #   '';
 
-    # Optional metadata
-    meta.description = "Gruvbox‐style GRUB theme";
-  };
+  #   # Optional metadata
+  #   meta.description = "Gruvbox‐style GRUB theme";
+  # };
 
-  hyperFluentSrc = pkgs.fetchFromGitHub {
-    owner  = "Coopydood";
-    repo   = "HyperFluent-GRUB-Theme";
-    rev    = "main";
-    sha256 = "16ai3nbscxq7gymadllp4gckaxy7w1vpp022b51zykl0ism1kalp";
-  };
+  # hyperFluentSrc = pkgs.fetchFromGitHub {
+  #   owner  = "Coopydood";
+  #   repo   = "HyperFluent-GRUB-Theme";
+  #   rev    = "main";
+  #   sha256 = "16ai3nbscxq7gymadllp4gckaxy7w1vpp022b51zykl0ism1kalp";
+  # };
 
-  hyperFluentTheme = pkgs.stdenv.mkDerivation {
-    name = "hyperfluent-grub-theme";
-    src  = hyperFluentSrc;
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out
-      cp -r $src/nixos/. $out/
-    '';
-  };
+  # hyperFluentTheme = pkgs.stdenv.mkDerivation {
+  #   name = "hyperfluent-grub-theme";
+  #   src  = hyperFluentSrc;
+  #   phases = [ "installPhase" ];
+  #   installPhase = ''
+  #     mkdir -p $out
+  #     cp -r $src/nixos/. $out/
+  #   '';
+  # };
 in {
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
     gfxmodeEfi = "auto";
     device = "nodev";
-    theme = "${hyperFluentTheme}/theme.txt";
+    theme = themeSource;
     # splashImage = "${gruvboxTheme}/grub-wallpaper.png";
     # font = "${gruvboxTheme}/FiraCode-Regular.pf2";
     # theme = "${gruvboxTheme}/theme.txt";
