e2747edd58050f91b812f5cfc013d68f8c58b45e
Peter Marks <peter.marks@betterment.com>
2026-04-21 16:39:33 -0400

    first crack at a POC image for the raspberry pi 5

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... } @ inputs:
@@ -20,28 +22,40 @@
       };
     };
   in {
-    nixosConfigurations.silicon = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit self nixpkgs nixpkgs-unstable home-manager; };
-      modules = [
-        ./hosts/silicon/configuration.nix
+    nixosConfigurations = {
+      silicon = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit self nixpkgs nixpkgs-unstable home-manager; };
+        modules = [
+          ./hosts/silicon/configuration.nix
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit unstable; };
-          home-manager.users.eaglerock = import ./home/eaglerock.nix;
-          # Back up conflicting files instead of failing
-          home-manager.backupFileExtension = "backup";
-        }
-        {
-          nixpkgs.config.allowUnfreePredicate = pkg:
-            builtins.elem ((pkg.pname or (pkg.name or ""))) [
-              "claude-code"
-            ];
-        }
-      ];
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit unstable; };
+            home-manager.users.eaglerock = import ./home/eaglerock.nix;
+            # Back up conflicting files instead of failing
+            home-manager.backupFileExtension = "backup";
+          }
+          {
+            nixpkgs.config.allowUnfreePredicate = pkg:
+              builtins.elem ((pkg.pname or (pkg.name or ""))) [
+                "claude-code"
+              ];
+          }
+        ];
+      };
+
+      hlc-501 = nixpks.lib.nixosSystem {
+        system = "aarch64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          raspberry-pi-nix.nixosModules.raspberry-pi-5
+          nixos-hardware.nixosModules.raspbery-pi-5
+          ./hosts/hlc-501/configuration.nix
+        ];
+      };
     };
   };
 }
